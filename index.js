831697ccf8d5c3d89fd8e47223e1386727b092c0
unknown <ibrahim15-4439@diu.edu.bd>
2024-11-16 01:31:48 +0600

    update

--- index.js
+++ index.js
@@ -1,57 +1,24 @@
-const apiKey = "aa6ea938956a570b463fa4a1a58898b7"
-
-const datasValue = (city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        .then(res => res.json())
-        .then(data => funtion(data)).catch(error => console.log(error))
-
-}
-
-const funtion = (data) => {
-    const cityName = document.getElementById('city')
-    const wather = document.getElementById('wather')
-    const watherSetuation = document.getElementById('wather-setuation')
-
-
-    if (typeof data.name === "undefined") {
-        cityName.innerText = 'Sorry No Result Found'
-        wather.innerText = ''
-        watherSetuation.innerText = ''
+function weatherApp() {
+    return {
+        apiKey: "aa6ea938956a570b463fa4a1a58898b7",
+        city: '',
+        weatherData: null,
+        error: '',
+        async fetchWeather() {
+            try {
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`);
+                const data = await response.json();
+                if (data.cod === 200) {
+                    this.weatherData = data;
+                    this.error = '';
+                } else {
+                    this.error = 'City not found. Please try again.';
+                    this.weatherData = null;
+                }
+            } catch (error) {
+                this.error = 'An error occurred. Please try again.';
+                this.weatherData = null;
+            }
+        }
     }
-    else {
-
-        cityName.innerText = data.name
-        wather.innerHTML = `${data.main.temp}  &deg;C`
-        watherSetuation.innerText = data.weather[0].description
-
-    }
-
-
-
 }
-
-const common = () => {
-
-    const inputFild = document.getElementById('input-field')
-    datasValue(inputFild.value)
-    console.log(inputFild.value)
-
-}
-
-
-
-
-// When I Click In A button 
-document.getElementById('input-field').addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        common()
-
-    }
-});
-
-
-document.getElementById('button').addEventListener('click', function () {
-    common()
-})
-
-
